322b99d12a58f7f4cfb4abcd0cc79797dc4887d3
Krish Goel <krishgoel20mar@gmail.com>
2023-10-24 13:28:41 +0530

    Changes made.

---
+++ static/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DateofBirth from './Components/DateofBirth';
+
+function App() {
+  return (
+    <div className="App">
+      <DateofBirth/>
+    </div>
+  );
+}
+
+export default App;
