7411ca4544c2272e1b58c44c797c039cf635432a
Krish Goel <krishgoel20mar@gmail.com>
2023-10-24 17:30:05 +0530

    Changes made.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Records from './Components/Records';
+
+function App() {
+  return (
+    <div className="App">
+      <Records/>
+    </div>
+  );
+}
+
+export default App;
